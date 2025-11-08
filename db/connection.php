a3283280f939f94fde5236bc171d25243a5c16e2
Rymndkraft <mstamaria.a12241624@umak.edu.ph>
2025-11-09 02:05:38 +0800

    fix: Update database connection to use  instead of getenv()

--- db/connection.php
+++ db/connection.php
@@ -1,6 +1,6 @@
 <?php
 // Use Heroku DATABASE_URL or JAWSDB_MARIA_URL if available, otherwise use local defaults
-$dbUrl = getenv('DATABASE_URL') ?: getenv('JAWSDB_MARIA_URL');
+$dbUrl = $_ENV['DATABASE_URL'] ?? $_ENV['JAWSDB_MARIA_URL'] ?? false;
 if ($dbUrl) {
     $url = parse_url($dbUrl);
     define('DB_HOST', $url['host']);
